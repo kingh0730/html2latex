7f9d15c87dc10e24031a81eae6c36df62326dcb2
Shangdian (King) Han <kingh0730@gmail.com>
2023-08-05 23:48:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ import os
 import subprocess
 
 # Third Party Stuff
-from pip.req import parse_requirements
+
+# from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    
 from setuptools import find_packages, setup
 from setuptools.command.install import install as InstallCommand
 from setuptools.command.test import test as TestCommand
